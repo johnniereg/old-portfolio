5f1b857a678a2561d7bf3d90c7841f793082c02f
Johnnie Regalado <johnniereg@gmail.com>
2018-01-19 23:19:16 +0000

    Setting up section state logic.
    Initial state is none for active section, then clicks set it to section titles.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,20 @@ import Section from './components/Section.jsx';
 import Footer from './components/Footer.jsx';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      activeSection: 'none'
+    };
+    this.selectSection = this.selectSection.bind(this);
+  }
+
+  selectSection(section) {
+    this.setState({
+      activeSection: section
+    });
+  }
+
   render() {
     return (
       <div className="App">
