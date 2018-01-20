e1770ec4f2774a4fba23496de222d0c0093a30dc
Johnnie Regalado <johnniereg@gmail.com>
2018-01-20 04:28:46 +0000

    Style tweaks

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -13,7 +13,7 @@ class Section extends Component {
         } else if ( this.props.activeSection === 'code') {
             return (
                 <div className="section container">
-                    <Subtitle title={this.props.activeSection} />
+                    <Subtitle title={this.props.activeSection} subtitleText="samples of my coding projects" />
                     <Entry entryName={"odd rad.io"} entryContent={"Listen to the strange sounds of Campus and Community Radio across Canada with this app. Built in React, Express, Node, PostgresQL and deployed on Heroku."} />
                     <Entry entryName={"house of noodles"} entryContent={"Restaurant app that allows customers to order online, using SMS confirmations for the restaurant and customer. Built in jQuery, Bootstrap, Express, Node, PostgreSQL, Twilio and deployed on Heroku."} />
                     <Entry entryName={"jungle"} entryContent={"eCommerce site which required additional features written on top of legacy code. Built in Ruby on Rails and tested using RSpec."} />
--- src/components/Subtitle.jsx
+++ src/components/Subtitle.jsx
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 class Subtitle extends Component {
     render(){
         return(
-            <div className="subtitle entry text-center">{this.props.title}</div>
+            <div className="subtitle entry text-center">
+                <div>{this.props.title}</div>
+                <div className="subtitle-text">{this.props.subtitleText}</div>
+            </div>
         )
     }
 
